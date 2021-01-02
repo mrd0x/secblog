1b64e147e6a650586bc61c735dbd2f2be52d1811
ahmad <ahmad-alsabagh@Hotmail.com>
2021-01-01 21:26:53 -0500

    Image quality fix

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -62,7 +62,7 @@ const Article = styled.article`
   blockquote {
     border-left: 5px solid #e0e6ed;
     line-height: 1.7;
-    margin: 15px 50px 15px 75px;
+    margin: 15px 50px 15px 45px;
     padding: 10px 10px 10px 15px;
 
     p {
