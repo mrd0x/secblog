53b9c45441e219f2bbb3fda84492935ed834dc07
mr.d0x <ahmad-alsabagh@hotmail.com>
2023-02-11 22:57:58 -0500

    test

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -7,7 +7,7 @@ function Pagination({previousUrl, nextUrl, isFirst = false, isLast = false}) {
   return (
     <Wrapper>
       {!isFirst &&
-        <Button to={previousUrl}>&larr; Newer Posts</Button>
+        <Button to={`/${previousUrl}`}>&larr; Newer Posts</Button>
       }
       {!isLast &&
         <Button to={nextUrl}>Older Posts &rarr;</Button>
